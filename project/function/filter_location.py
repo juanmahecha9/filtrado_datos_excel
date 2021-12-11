874c5b568637895e07494d7a5d907a0d77d3a456
Juan David Mahecha C <60445489+juanmahecha9@users.noreply.github.com>
2021-12-10 22:31:29 -0500

    function update

--- project/function/filter_location.py
+++ project/function/filter_location.py
@@ -5,37 +5,30 @@ with open('project/function/config/location.json') as file:
     data = json.load(file)
 
 
-def filter_location(path_in,file_name, path_out):
+def filter_location(params):
+    text = params.split(";")
+    path_temp = text[0]
+    file_name = text[1]
     # Dividir el nombre del archivo para captura la ciudad
-    city = file_name.split()
+    city = file_name.split(" ")
     print(file_name)
     # Lectura del archivo
-    input_cols = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
-    df = pd.read_excel(path_in+file_name+".xlsx", index_col=0, header=0, usecols=input_cols)
+    input_cols = list(range(20))
+    df = pd.read_excel(path_temp+"/"+file_name+".xlsx", index_col=0, header=0, usecols=input_cols)
     
     state = []
 
-    if(city[1] == "RN"):
-       print(f"Ejecutando filtro de locacion ",city[1], " Para los sitios de entrenamiento de: ",data['rn'])
-       for local in data['rn']:
-           df1 = df[df["Local de Treinamento"] == local]
-           
-           # Filas y columnas para evaluar si hay o no informacion filtrada
-           (row,col) = df1.shape
-           if(row < 2):
-                state.append({"local": "No hay datos relacionado con: "+local})
-           if(row >1):
-                df1.to_excel(path_out+local+".xlsx", header = True, index= True)
-                state.append({"local": "Filtrados por: "+local})
-    else:
-        print(f"Ejecutando filtro de locacion ",city[1], " Para los sitios de entrenamiento de: ",data['sp'])
-        for local in data['sp']:
-            df1 = df[df["Local de Treinamento"] == local]
-            
-            (row,col) = df1.shape
-            if(row < 2):
-                state.append({"local": "No hay datos relacionado con: "+local})
-            if(row >1):
-                df1.to_excel(path_out+local+".xlsx", header = True, index= True)
-                state.append({"local": "Filtrados por: "+local})
-    return state
+    to_loop = data['rn'] if city[1] == 'RN' else data['sp']
+    print(f"Ejecutando filtro de locacion ",city[1], " Para los sitios de entrenamiento de: ",data['rn'])
+    for local in to_loop:
+        df_local = df[df["Local de Treinamento"] == local]
+        
+        # Filas y columnas para evaluar si hay o no informacion filtrada
+        rows = len(df)
+        if(rows < 2):
+            state.append({"local": "No hay datos relacionado con: "+local})
+        else:
+            df_local["SOLICITAÇÃO DE DNA"] = "AgentBot"
+            df_local.to_excel(path_temp+"/"+local+".xlsx", header = True, index= True)
+            state.append({"local": "Filtrados por: "+local})
+    return json.dumps(state)
